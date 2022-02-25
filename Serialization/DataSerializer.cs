c0945eba61b758c4dec7211134c49b7ea406e202
dmitr <dmitr@DKRASIEV>
2022-02-25 12:50:54 +0400

    Add project files.

---
+++ Serialization/DataSerializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    internal class DataSerializer
+    {
+        public void BinarySerialize(object data, string filePath)
+        {
+            BinaryFormatter bf = new();
+            if (File.Exists(filePath)) File.Delete(filePath);
+
+            using (FileStream fs = File.Create(filePath))
+            {
+                bf.Serialize(fs, data);
+            }
+        }
+
+        public void XMLSerialize(object data, string filePath)
+        {
+
+        }
+
+        public void JSONSerialize(object data, string filePath)
+        {
+
+        }
+    }
+}
---
+++ Serialization/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Serialization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DataSerializer _serializer;
+        private object _data;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _serializer = new DataSerializer();
+        }
+
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                string filePath = ofd.FileName;
+                using (FileStream fs = new FileStream(filePath, FileMode.Open))
+                {
+                    StreamReader sr = new StreamReader(fs);
+                    _data = sr.ReadToEnd();
+                }
+                MessageBox.Show("Выбран файл: " + filePath, "Succes");
+            }
+            else
+            {
+                ShowError();
+            }
+        }
+
+        private void BinarySerialization_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog ofd = new SaveFileDialog();
+
+            if (ofd.ShowDialog() == true)
+            {
+                _serializer.BinarySerialize(_data, ofd.FileName);
+            }
+            else
+            {
+                ShowError();
+            }
+        }
+
+        private void XMLSerialization_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog ofd = new SaveFileDialog();
+
+            if (ofd.ShowDialog() == true)
+            {
+                _serializer.XMLSerialize(_data, ofd.FileName);
+            }
+            else
+            {
+                ShowError();
+            }
+        }
+
+        private void JSONSerialization_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog ofd = new SaveFileDialog();
+
+            if (ofd.ShowDialog() == true)
+            {
+                _serializer.JSONSerialize(_data, ofd.FileName);
+            }
+            else
+            {
+                ShowError();
+            }
+        }
+
+        private void ShowError() 
+        {
+            MessageBox.Show("Error");
+        }
+    }
+}
